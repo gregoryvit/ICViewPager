d6a1e4b904aac788b91578fb633ab4849ba526bb
gregoryvit <gregoryvit@gmail.com>
2015-12-17 20:11:00 +0100

    topLayoutGuide fixes

--- ICViewPager/ICViewPager/ViewPagerController.m
+++ ICViewPager/ICViewPager/ViewPagerController.m
@@ -193,14 +193,7 @@
 }
 
 - (void)layoutSubviews {
-    
-    CGFloat topLayoutGuide = 0.0;
-    if (IOS_VERSION_7) {
-        topLayoutGuide = 20.0;
-        if (self.navigationController && !self.navigationController.navigationBarHidden) {
-            topLayoutGuide += self.navigationController.navigationBar.frame.size.height;
-        }
-    }
+    CGFloat topLayoutGuide = [self.topLayoutGuide length];
     
     CGRect frame = self.tabsView.frame;
     frame.origin.x = 0.0;
